81f46cfc051c5fd1e3111c343267a2c1f492b769
paul guichon <paul.guichon@iedparis8.net>
2015-02-24 16:53:44 +0100

    get_recipients

--- duck_utils/utils.py
+++ duck_utils/utils.py
@@ -1,5 +1,14 @@
 __author__ = 'paulguichon'
+from django.conf import settings
 
 
 def email_ied(individu):
     return str(individu.cod_etu) + '@foad.iedparis8.net'
+
+
+def get_recipients(individu, cod_anu):
+    if settings.DEBUG:
+        recipients = (settings.EMAIL_DEV, )
+    else:
+        recipients = (individu.cod_ind.get_email(individu.cod_anu.cod_anu), email_ied(individu.cod_ind))
+    return recipients
