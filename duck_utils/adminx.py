e41a3d0021878b5f64af951423a9e2b9a5fa9220
Alexandre Parent <barbak@gmail.com>
2015-02-25 11:39:39 +0100

    Reversion sur TemplateHtmlModel.

--- duck_utils/adminx.py
+++ duck_utils/adminx.py
@@ -1,4 +1,5 @@
 from mailrobot.models import MailBody, Mail, Address, Signature
+import reversion
 from duck_utils.models import MailProperty, Property, TemplateHtmlModel
 import xadmin
 from xadmin.views import filter_hook
@@ -50,7 +51,7 @@ class MailBodyAdmin(object):
             return self.readonly_fields
 
 class TemplateHtmlModelAdmin(object):
-    pass
+    reversion_enable = True
 
 xadmin.site.register(MailBody, MailBodyAdmin)
 xadmin.site.register(Mail)
@@ -58,3 +59,4 @@ xadmin.site.register(Address)
 xadmin.site.register(Signature)
 xadmin.site.register(Property)
 xadmin.site.register(TemplateHtmlModel, TemplateHtmlModelAdmin)
+xadmin.plugins.xversion.register_models()
