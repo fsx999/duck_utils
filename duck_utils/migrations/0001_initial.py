5428dfb2e249deff21fe30d51471108cfaea57f7
paul guichon <paul.guichon@iedparis8.net>
2015-03-31 15:08:53 +0200

    migration django

--- duck_utils/migrations/0001_initial.py
+++ duck_utils/migrations/0001_initial.py
@@ -1,56 +1,81 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Property'
-        db.create_table(u'duck_utils_property', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('label', self.gf('django.db.models.fields.CharField')(max_length=60)),
-        ))
-        db.send_create_signal(u'duck_utils', ['Property'])
+    dependencies = [
+        ('mailrobot', '__first__'),
+        ('django_apogee', '0001_initial'),
+    ]
 
-        # Adding model 'MailProperty'
-        db.create_table(u'duck_utils_mailproperty', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('mail', self.gf('django.db.models.fields.related.OneToOneField')(related_name='mail_property', unique=True, to=orm['mailrobot.MailBody'])),
-            ('property', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['duck_utils.Property'])),
-        ))
-        db.send_create_signal(u'duck_utils', ['MailProperty'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Property'
-        db.delete_table(u'duck_utils_property')
-
-        # Deleting model 'MailProperty'
-        db.delete_table(u'duck_utils_mailproperty')
-
-
-    models = {
-        u'duck_utils.mailproperty': {
-            'Meta': {'object_name': 'MailProperty'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'mail': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'mail_property'", 'unique': 'True', 'to': u"orm['mailrobot.MailBody']"}),
-            'property': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['duck_utils.Property']"})
-        },
-        u'duck_utils.property': {
-            'Meta': {'object_name': 'Property'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'label': ('django.db.models.fields.CharField', [], {'max_length': '60'})
-        },
-        u'mailrobot.mailbody': {
-            'Meta': {'object_name': 'MailBody'},
-            'body': ('django.db.models.fields.TextField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '40'}),
-            'subject': ('django.db.models.fields.CharField', [], {'max_length': '66'})
-        }
-    }
-
-    complete_apps = ['duck_utils']
+    operations = [
+        migrations.CreateModel(
+            name='EtapeSettings',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('cod_anu', models.IntegerField()),
+                ('contact_info', models.TextField()),
+                ('etape', models.ForeignKey(to='django_apogee.Etape')),
+            ],
+            options={
+                'verbose_name_plural': 'etape settings',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='MailProperty',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('help_text', models.TextField(blank=True)),
+                ('mail', models.OneToOneField(related_name='mail_property', to='mailrobot.MailBody')),
+            ],
+            options={
+                'verbose_name_plural': 'Mail properties',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Property',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('label', models.CharField(max_length=60)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Salle',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('label', models.CharField(max_length=120)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TemplateHtmlModel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=64)),
+                ('label', models.CharField(max_length=120)),
+                ('help_text', models.TextField(null=True, blank=True)),
+                ('content', models.TextField()),
+                ('context_test', models.TextField(null=True, blank=True)),
+                ('property', models.ForeignKey(blank=True, to='duck_utils.Property', null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='mailproperty',
+            name='property',
+            field=models.ForeignKey(to='duck_utils.Property'),
+            preserve_default=True,
+        ),
+    ]
